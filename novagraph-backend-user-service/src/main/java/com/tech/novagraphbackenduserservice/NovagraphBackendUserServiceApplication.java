1caec8752d1b5e99a4495563482e81c03d4d99cc
REMON\Remon <982581058@qq.com>
2025-11-24 17:33:34 +0800

    灵境故事一期 添加common模块

--- novagraph-backend-user-service/src/main/java/com/tech/novagraphbackenduserservice/NovagraphBackendUserServiceApplication.java
+++ novagraph-backend-user-service/src/main/java/com/tech/novagraphbackenduserservice/NovagraphBackendUserServiceApplication.java
@@ -2,7 +2,9 @@ package com.tech.novagraphbackenduserservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("com.tech")
 @SpringBootApplication
 public class NovagraphBackendUserServiceApplication {
 
--- novagraph-backend-user-service/src/main/java/com/tech/novagraphbackenduserservice/interfaces/controller/UserController.java
+++ novagraph-backend-user-service/src/main/java/com/tech/novagraphbackenduserservice/interfaces/controller/UserController.java
@@ -1,5 +1,9 @@
 package com.tech.novagraphbackenduserservice.interfaces.controller;
 
+import com.tech.novagraphbackendcommon.common.BaseResponse;
+import com.tech.novagraphbackendcommon.common.ResultUtils;
+import com.tech.novagraphbackendcommon.exception.BusinessException;
+import com.tech.novagraphbackendcommon.exception.ErrorCode;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @GetMapping("/hello")
-    public String demo(){
-        return "Hello World";
+    public BaseResponse<String> demo(){
+        return ResultUtils.success("Hello World");
+    }
+
+    @GetMapping("/error")
+    public BaseResponse<String> error(){
+        throw new BusinessException(ErrorCode.SYSTEM_ERROR);
     }
 }
